09c77fa0f7a112c6d67e7c9a3020fb51cf9a541a
刘怡豪 <mildwind2022@gmail.com>
2025-12-12 23:16:19 +0800

    更新README

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -73,7 +73,7 @@ static std::shared_ptr<Sudoku> GetSudoku(std::istream &in)
 
 int main()
 {
-    std::filesystem::path path(R"(odd_even\sudoku5.txt)");
+    std::filesystem::path path(R"(sudoku\sudoku5.txt)");
     if (!std::filesystem::exists(path))
     {
         std::cerr << "Path Not Found: " << path << std::endl;
